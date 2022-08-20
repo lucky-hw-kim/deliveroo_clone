5164aad6ef08d53908c595449f9425de6e2179fd
lucky <lucky.hw.kim@gmail.com>
2022-08-19 20:23:47 -0700

    env

--- sanity.js
+++ sanity.js
@@ -6,7 +6,7 @@ const client = sanityClient({
   dataset: "production",
   useCdn: true,
   apiVersion: "2021-10-21",
-  token: "sk3y6kJNhsMrLnfziPaXLlGo9gBDtdge8lzKnlArI3mL1dMLdVr0UFtxhxUEK5WCJcddkFbNxXNfF6oZhsEG8D22GAFjXfF3VwN1pxliznk4GzzSOtnUzsNYmk9ZWHONcoNWyaBKd0F8x8V1y9YyrOBEvsiLKw21y0T1i6kXYiyUPr6dH6bI"
+  token: process.env.REACT_APP_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client);
